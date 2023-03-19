a0bad104a5e5db7faed8730b268f7eaf1f30234f
HyungJinHan <han1210_36@naver.com>
2023-03-20 04:50:24 +0900

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,6 +73,30 @@ const Navbar = () => {
               setToggle(!toggle);
             }}
           />
+
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex sm:flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } ${
+                    styles.animate
+                  } hover:text-white text-[18px] font-medium cursor-pointer`}
+                  onClick={() => {
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
