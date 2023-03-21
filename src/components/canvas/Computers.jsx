7b6f9200312b69b980b9c0b3acabf32e4c338807
HyungJinHan <han1210_36@naver.com>
2023-03-21 17:07:53 +0900

    데스크탑 모델링 불러오기 성공

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,41 @@ import CanvasLoader from "../Loader";
 const Computers = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
-  return <div>Computers</div>;
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
 };
 
-export default Computers;
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      // 특정한 최적화 수행을 막지만 WebGL이 캔버스를 지우지 않도록 하는 작업
+    >
+      <Suspense>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
