5636921fa4b3ab35670a6fbd02308197fd1af4a7
HyungJinHan <han1210_36@naver.com>
2023-03-22 03:45:42 +0900

    contact 행성 불러오기 성공, 별 배경 작업 중

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useRef } from "react";
-import { motion } from "framer-motion";
+import { animate, motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
@@ -22,7 +22,72 @@ const Contact = () => {
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
-      <motion.div variants={slideIn("left", "tween")}></motion.div>
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Please contact me</p>
+        <h2 className={styles.sectionHeadText}>Contact</h2>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input
+              required
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="이름을 입력해주세요."
+              className="bg-tertiary rounded-lg py-4 px-6 placeholder:text-secondary text-white outline-none border-none font-medium"
+            />
+          </label>
+
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input
+              required
+              type="text"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="이메일을 입력해주세요."
+              className="bg-tertiary rounded-lg py-4 px-6 placeholder:text-secondary text-white outline-none border-none font-medium"
+            />
+          </label>
+
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+              required
+              rows="7"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="보낼 내용을 입력해주세요."
+              className="bg-tertiary rounded-lg py-4 px-6 placeholder:text-secondary text-white outline-none border-none font-medium"
+            />
+          </label>
+
+          <button
+            type="submit"
+            className={`bg-tertiary py-3 px-9 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl hover:scale-[98%] hover:shadow-sm ${styles.animate}`}
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
+      </motion.div>
     </div>
   );
 };
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  return (
-    <div>Earth</div>
-  )
-}
+  const earth = useGLTF("./planet/scene.gltf");
 
-export default Earth
+  return (
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
+};
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}
+      gl={{ preserveDrawingBuffer: true }}
+      // 특정한 최적화 수행을 막지만 WebGL이 캔버스를 지우지 않도록 하는 작업
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
