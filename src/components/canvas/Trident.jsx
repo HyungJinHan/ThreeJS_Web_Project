3cc753c71607170ac3cb6dccaa60ca21fcf0c8ea
HyungJinHan <han1210_36@naver.com>
2023-04-07 18:12:05 +0900

    3dm 기준점 0, 0, 0 변경으로 문제 해결

--- src/components/canvas/Trident.jsx
+++ src/components/canvas/Trident.jsx
@@ -30,8 +30,8 @@ function Model({ url, ...props }) {
       />
       <primitive
         object={model}
-        scale={0.05}
-        position={[0, -5, 2]}
+        scale={0.02}
+        position={[0, -20, 2]}
         rotation={[-1.56, 0, 0]}
       />
     </mesh>
@@ -43,19 +43,19 @@ function Trident() {
     <Canvas
       frameloop="demand"
       // shadows
-      camera={{ position: [50, 3, 5], fov: 20 }}
+      camera={{ position: [200, 3, 5], fov: 20 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={true}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
+          // maxPolarAngle={Math.PI / 2}
+          // minPolarAngle={Math.PI / 2}
           autoRotate
           autoRotateSpeed={1}
           rotateSpeed={0.3}
         />
-        <Model url="/trident_motor_note.3dm" />
+        <Model url="/trident_move.3dm" />
         {/* <Model url="/IJNnagato6.3dm" /> */}
       </Suspense>
     </Canvas>
