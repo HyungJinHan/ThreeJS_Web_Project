2199853d0e59d4bf6f4fb8857424ed57d87f7e80
HyungJinHan <han1210_36@naver.com>
2023-04-14 18:38:45 +0900

    .

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,6 +5,7 @@ import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
+  // const computer = useGLTF("./untitled.glb");
 
   return (
     <mesh>
